104bb3a645653f21b6887ad4e5b29ef2f9b6e713
grphm-robot <robot@grapheme.ru>
2014-11-17 11:48:46 +0000

    server

--- app/config/l24/database.php
+++ app/config/l24/database.php
@@ -5,7 +5,7 @@ return array(
 	'connections' => array(
 		'mysql' => array(
 			'driver'    => 'mysql',
-			'host'      => 'localhost',
+			'host'      => 'mysql60.1gb.ru',
 			'database'  => 'gb_x_infind89',
 			'username'  => 'gb_x_infind89',
 			'password'  => '4c951c5aty',
