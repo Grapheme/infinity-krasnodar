5fef82690605da153cc11b148b57ab5bf2bbe15a
Владимир <vkharseev@gmail.com>
2014-12-25 16:20:33 +0300

    mail config

--- app/config/mail.php
+++ app/config/mail.php
@@ -5,15 +5,15 @@ return array(
     'feedback_mail' => 'support@grapheme.ru',
     'feedback_name' => 'grapheme.ru',
 
-    'sendto_mail' => 'support@grapheme.ru',
-    #'sendto_mail' => 'info@infiniti-krasnodar.ru',
+    #'sendto_mail' => 'support@grapheme.ru',
+    'sendto_mail' => 'kr.infiniti-sales@gedon.ru ',
 
 	'driver' => 'smtp',
 	'host' => 'in.mailjet.com',
 	'port' => 587,
 	'from' => array(
 		'address' => 'no-reply@infiniti-krasnodar.ru',
-		'name' => 'Infinity'
+		'name' => 'INFINITI'
 	),
 	'username' => '0d8dd8623bd38b41c43683c41c0558eb',
 	'password' => '465c500abd5f680f0b20405deb967b36',
