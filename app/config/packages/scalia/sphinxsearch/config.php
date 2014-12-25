8de8abc38a7058db26c11b0cf749a524331d0b94
Владимир <vkharseev@gmail.com>
2014-12-25 15:34:48 +0300

    Krasnodar

--- app/config/packages/scalia/sphinxsearch/config.php
+++ app/config/packages/scalia/sphinxsearch/config.php
@@ -4,10 +4,10 @@ return array (
     'host'    => '127.0.0.1',
     'port'    => 3312,
     'indexes' => array (
-        'channelsIndexInfinity' => array('table'=>'channels','column'=>'id','modelname'=>'Channel'),
-        'productsIndexInfinity' => array('table'=>'products_meta','column'=>'product_id','modelname'=>'ProductsMeta'),
-        'productsAccessibilityIndexInfinity' => array('table'=>'products_accessories','column'=>'id','modelname'=>'ProductAccessory'),
-        'newsIndexInfinity' => array('table'=>'i18n_news_meta','column'=>'news_id','modelname'=>'I18nNewsMeta'),
-        'pagesIndexInfinity' => array('table'=>'i18n_pages_meta','column'=>'page_id','modelname'=>'I18nPageMeta')
+        'channelsIndexInfinityKrasnodar' => array('table'=>'channels','column'=>'id','modelname'=>'Channel'),
+        'productsIndexInfinityKrasnodar' => array('table'=>'products_meta','column'=>'product_id','modelname'=>'ProductsMeta'),
+        'productsAccessibilityIndexInfinityKrasnodar' => array('table'=>'products_accessories','column'=>'id','modelname'=>'ProductAccessory'),
+        'newsIndexInfinityKrasnodar' => array('table'=>'i18n_news_meta','column'=>'news_id','modelname'=>'I18nNewsMeta'),
+        'pagesIndexInfinityKrasnodar' => array('table'=>'i18n_pages_meta','column'=>'page_id','modelname'=>'I18nPageMeta')
     )
 );
