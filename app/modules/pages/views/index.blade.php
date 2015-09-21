02415f51ae116feaf5e532d9c8268b6e099203f2
Kirill Fadeev <fk@realitygroup.ru>
2015-09-21 13:00:00 +0300

    styles-fix

--- app/modules/pages/views/index.blade.php
+++ app/modules/pages/views/index.blade.php
@@ -4,12 +4,18 @@
 	.slider-link{
 		display: inline-block;
 		color: #FFF !important;
-		border: 2px solid #FFF;
+		/*border: 2px solid #FFF;*/
+		background-color: #613f7e;
 		padding: 0 .9375rem;
 		line-height: 3.3125rem;
 		text-decoration: none;
 		margin-top: 20px;
-		box-shadow: 2px 2px 4px rgba(0,0,0,0.75);
+		text-shadow: none;
+		/*box-shadow: 2px 2px 4px rgba(0,0,0,0.75);*/
+	}
+
+	.slider-link:hover {
+		background-color: rgba(97, 63, 126, 0.8);
 	}
 </style>
 @stop
